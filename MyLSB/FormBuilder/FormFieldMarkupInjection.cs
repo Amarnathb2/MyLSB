1030fca452c8afd1d00dfc74be3d142bd50d2e31
mannis <mannis@zaginteractive.com>
2021-06-29 10:24:32 -0400

    set correct class on checkbox input

--- MyLSB/FormBuilder/FormFieldMarkupInjection.cs
+++ MyLSB/FormBuilder/FormFieldMarkupInjection.cs
@@ -167,6 +167,7 @@ namespace MyLSB.FormBuilder.FormBuilderCustomizations
                 // udpate wrapping markup
                 e.Configuration.LabelWrapperConfiguration.ElementName = "legend";
                 //e.Configuration.LabelHtmlAttributes["role"] = "none";
+                e.Configuration.EditorHtmlAttributes["class"] = "custom-control-input";
 
                 // add class for type of validation
                 e.Configuration.EditorHtmlAttributes["class"] += " zag-validation-input-checkboxes";
