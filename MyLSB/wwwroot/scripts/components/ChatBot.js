3732036fc5bf25866a6c1f3820f026c3200b7f2c
Pradeep Pentakota <pradeeppentakota@hotmail.com>
2021-12-02 00:32:33 +0530

    css and script changes for webchat - https://zag.my.workfront.com/issue/618427ee008b6d84a063f4ce87f1002a

---
+++ MyLSB/wwwroot/scripts/components/ChatBot.js
@@ -0,0 +1,34 @@
+﻿﻿var App = App || {};
+
+App.ChatBot = (function ($) {
+    "use strict";
+
+    function init() {
+        window.onload = function () {
+           
+            if ($('#check-chat-available').length) {
+                checkContainerdiv1();
+            }
+        };
+       
+    }
+    function loadAfterTime() {
+        $('#five9-maximize-button').attr('tabindex', '0');
+        $('#five9-maximize-button').attr('aria-label', 'chat');      
+    }
+    function checkContainerdiv1() {
+        if ($('#five9-maximize-button').is(':visible')) {
+            loadAfterTime();
+        } else {
+            setTimeout(checkContainerdiv1, 50); //wait 50 ms, then try again
+        }
+    }    
+    return {
+        init: init
+    };
+
+})(jQuery);
+
+$(function () {
+    App.ChatBot.init();
+});
